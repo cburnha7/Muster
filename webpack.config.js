c457f8278f3a63b692d120707888918ec0ffd8c7
Charles Burnham <you@example.comedwinburham@gmail.com>
2026-03-19 16:29:54 -0400

    calender sizing

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,14 @@ module.exports = async function (env, argv) {
     argv
   );
 
+  // Polyfill Node built-ins that some dependencies expect in the browser
+  config.resolve.fallback = {
+    ...(config.resolve.fallback || {}),
+    url: require.resolve('url/'),
+    crypto: false,
+    stream: false,
+    buffer: false,
+  };
+
   return config;
 };
